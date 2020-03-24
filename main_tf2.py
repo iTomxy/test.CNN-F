4557ee658b9c1e3911f2b0cd94c4042b45535b14
iTom <hackeritom@163.com>
2020-03-24 21:25:42 +0800

    change log

--- main_tf2.py
+++ main_tf2.py
@@ -22,7 +22,7 @@ args = parser.parse_args()
 mnist = K.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
-print("data shape:", x_train.dtype, x_train.shape, y_test.shape)
+print("data shape:", type(x_train), x_train.shape, y_test.shape)
 
 # Add a channels dimension
 x_train = x_train[..., tf.newaxis]
